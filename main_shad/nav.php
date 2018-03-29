8bc817a0e0eebd3b51cedeb82e421ccaed401580
Andriy503 <andriy.sokolovskiy.97@mail.ru>
2018-03-29 20:53:48 +0300

    sort tovar

---
+++ main_shad/nav.php
@@ -0,0 +1,31 @@
+<div id="block_user">
+	<div class="corner"><img src="images/bulleticon.png"></div>
+		<ul>
+			<li><i class="material-icons">settings</i><span class="setting nav_seting">Налаштування</span></li>
+			<li><i class="material-icons">exit_to_app</i><span class="logout nav_seting">Вийти</span></li>
+		</ul>
+</div>
+
+<nav>
+	<a href="/"><i class="large material-icons">home</i>Головна</a>
+	<a href="index.php?go=news"><i class="large material-icons">sentiment_very_satisfied</i>Новинки</a>
+	<a href="index.php?go=liders"><i class="large material-icons">insert_chart</i>В тренді</a>
+	<a href="index.php?go=sale"><i class="large material-icons">local_mall</i>Акції</a>
+ 	<?php 
+		if($_GET['action']){
+			$class_action = "action_basket";
+		}
+   	 ?>
+	
+	<a href="cart.php?action=oneclick" class="basket_icon" id=<?php echo $class_action;?> ><i class="large material-icons">shopping_cart</i>Корзина пуста</a>
+	<!-- local_grocery_store // кошик -->
+<div id="search">
+	<form method="get" action="index.php" id="search_form">
+		<input type="text" name="search_name" id="search_name" placeholder="Пошук..." value='<?php echo $_GET['search_name']; ?>'>
+		<input type="submit" name="send_search" class="send_search" value="Знайти">
+	</form>
+		<ul id="result_search">
+			
+		</ul>
+	</div>
+</nav>
---
+++ main_shad/script.js
@@ -0,0 +1,147 @@
+$(document).scroll(function() {
+	if($(document).scrollTop () > $('header').height () + 10)
+	$('nav').addClass('fixed');
+	else 
+		$('nav').removeClass('fixed');
+});
+
+$(document).ready(function () {
+	$(".new_sticker").jCarouselLite({
+		vertical: true,
+		hoverPause:true,
+		btnPrev: ".drop_up",
+		btnNext: ".drop_down",
+		visible: 3,
+		auto:3000,
+		speed:500
+	});
+
+	// Вигляд кнопок
+	$("#icon_grid").click(function() {
+		$("#block_tovar_list").hide();
+		$("#block_tovar_grid").show();
+
+		$("#icon_grid").attr("src", "images/icon_grid_active.png");
+		$("#icon_list").attr("src", "images/icon_list.png");
+
+		$.cookie('select_style', 'grid');
+	});
+
+	$("#icon_list").click(function() {
+		$("#block_tovar_grid").hide();
+		$("#block_tovar_list").show();
+
+		$("#icon_list").attr("src", "images/icon_list_active.png");
+		$("#icon_grid").attr("src", "images/icon_grid.png");
+
+		$.cookie('select_style', 'list');
+	});
+
+	if($.cookie('select_style') == 'grid'){
+		$("#block_tovar_list").hide();
+		$("#block_tovar_grid").show();
+
+		$("#icon_grid").attr("src", "images/icon_grid_active.png");
+		$("#icon_list").attr("src", "images/icon_list.png");
+	} else if($.cookie('select_style') == 'list') {
+		$("#block_tovar_grid").hide();
+		$("#block_tovar_list").show();
+
+		$("#icon_list").attr("src", "images/icon_list_active.png");
+		$("#icon_grid").attr("src", "images/icon_grid.png");
+	} 
+
+	if(!$.cookie('select_style')){
+		$("#icon_grid").attr("src", "images/icon_grid_active.png");
+	} 
+
+	$('#select_sort').click(function() {
+		$("#sorting_list").slideToggle(200);
+	});
+
+	//*********************Акардіон
+	$('#block_category > ul > li > a').click(function() {
+		if($(this).attr('class') != 'active') {
+			$('#block_category > ul > li > ul').slideUp(400);
+			$(this).next().slideToggle(400);
+
+				$('#block_category > ul > li > a').removeClass('active');
+				$(this).addClass('active');
+				$.cookie('select_cat', $(this).attr('id'));
+		} else {
+			$('#block_category > ul > li > a').removeClass('active');
+			$('#block_category > ul > li > ul').slideUp(400);
+			$.cookie('select_cat', '');
+		}
+	});
+
+	if($.cookie('select_cat') != '') {
+		$('#block_category > ul > li > #'+$.cookie('select_cat')).addClass('active').next().show();
+	}
+
+	$("#auth_user_info").toggle(function() {
+		$("#block_user").fadeIn(400);
+	}, function() {
+		$("#block_user").fadeOut(400);
+	});
+
+	// ajax запит на вихід із акаунта
+	$(".logout").click(function() {
+		$.ajax({
+			type: "POST",
+			url: "include/functions/sign_in_close_session.php",
+			dataType: "html",
+			cache: false,
+			success: function(data) {
+				if(data == "logout"){
+					location.reload();
+				}
+			}
+		});
+	});
+	
+	$(".setting").click(function() {
+		window.location.replace("http://logoshop.ua/setting.php");
+	});
+
+
+	$("#search_form").validate({
+		rules: {
+			search_name: {
+				maxlength: 50
+			}
+		},
+		messages: {
+			search_name: {
+				maxlength: "<span class='mes_search'>поле задовге!</span>"
+			}
+		}
+	});
+
+	$("#search_name").bind('textchange', function() {
+		var input_search = $("#search_name").val();
+
+		if(input_search.length > 2) {
+			$.ajax({
+				type: "POST",
+				url: "include/functions/search_api.php",
+				data: "text="+input_search,
+				dataType: "html",
+				cache: false,
+				success: function(data) {
+					if(data > ''){
+						$("#result_search").show().html(data);						
+					} else {
+						$("#result_search").hide();
+					}
+				}
+			});
+		} else {
+			$("#result_search").hide();
+		}
+	});
+
+
+});
+
+
